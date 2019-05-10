4bab38029f0ccda0c08840df82f69333fd806250
Unknown <flisadora@gmail.com>
2019-05-10 16:04:32 +0100

    creation activity Anuncios

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,19 +1,32 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this,"Your Message", Toast.LENGTH_LONG).show();
+        Button buttonOne = findViewById(R.id.button5);
+        buttonOne.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("Button Clicked");
+                Intent activity2Intent = new Intent(getApplicationContext(), Anuncios.class);
+                startActivity(activity2Intent);
+            }
+        });
     }
 
     @Override
@@ -23,5 +36,9 @@ public class MainActivity extends AppCompatActivity {
         Toast toast = Toast.makeText(this, "menu opened", Toast.LENGTH_LONG);
         toast.show();
         return true;
+    }
+
+    public void launchAnuncios(View view) {
+        //Log.d(LOG_TAG, "Button clicked!");
     }
 }
