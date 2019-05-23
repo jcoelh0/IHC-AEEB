fd944ac7cd4c5ca93caf8ed505cf28c70ff0a40e
Unknown <flisadora@gmail.com>
2019-05-23 12:49:15 +0100

    update activity_meus_dados

--- app/src/main/java/com/example/myapplication/Anuncios.java
+++ app/src/main/java/com/example/myapplication/Anuncios.java
@@ -1,15 +1,11 @@
 package com.example.myapplication;
 
 import android.content.Intent;
-import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.CompoundButton;
 import android.widget.TextView;
-import java.lang.Object;
 
 
 public class Anuncios extends AppCompatActivity {
@@ -24,7 +20,7 @@ public class Anuncios extends AppCompatActivity {
         setContentView(R.layout.activity_anuncios);
 
         TextView anuncio1 = (TextView) findViewById(R.id.textView4);
-        TextView dataAnuncio1 = (TextView) findViewById(R.id.textView5);
+        TextView dataAnuncio1 = (TextView) findViewById(R.id.textView7);
         TextView anuncio2 = (TextView) findViewById(R.id.textView6);
         TextView dataAnuncio2 = (TextView) findViewById(R.id.textView7);
         //TextView anuncio3 = (TextView) findViewById(R.id.textView8);
